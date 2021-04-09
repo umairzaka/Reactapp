68fb7d8b11d3bb1fea75c50287e59e6601d4c7fb
Umair Zaka <82205655+umairzaka@users.noreply.github.com>
2021-04-09 15:11:15 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This is my first react application</p>
+      <a href='www.facebook.com'>facebook</a>
+    </div>
+  );
+}
+
+export default App; 
